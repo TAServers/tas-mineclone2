18eaabc246a5b0fa1fcc95bafd123ae281f0702b
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2023-07-16 13:10:30 -0700

    refactor: fix markups and clean up control table when players rejoin

--- packages/tas_keybind/init.lua
+++ packages/tas_keybind/init.lua
@@ -8,16 +8,18 @@ local controlListeners = {}
 
 ---@enum tas.CONTROLS
 tas.CONTROLS = {
-	up = 0,
-	down = 1,
-	left = 2,
-	right = 3,
-	jump = 4,
-	aux1 = 5,
-	sneak = 6,
-	dig = 7,
-	place = 8,
-	zoom = 9,
+	UP = 0,
+	DOWN = 1,
+	LEFT = 2,
+	RIGHT = 3,
+	JUMP = 4,
+	AUX1 = 5,
+	SNEAK = 6,
+	--- Equivalent to mouse button 1 (left click)
+	DIG = 7,
+	--- Equivalent to mouse button 2 (right click)
+	PLACE = 8,
+	ZOOM = 9,
 }
 
 for _, control in pairs(tas.CONTROLS) do
@@ -36,16 +38,14 @@ local function callControlListeners(player)
 	local changedControls = getControlChanges(player:get_player_name(), newControls)
 
 	for controlName, controlBitIndex in pairs(tas.CONTROLS) do
-		local controlBitmask = bit.rshift(1, controlBitIndex)
+		local controlBitmask = bit.lshift(1, controlBitIndex)
 		local changed = bit.band(changedControls, controlBitmask) ~= 0
 		local pressed = bit.band(newControls, controlBitmask) ~= 0
 
 		if changed then
 			local listeners = controlListeners[controlBitIndex]
-			if listeners then
-				for callback in pairs(listeners) do
-					callback(player, pressed)
-				end
+			for callback in pairs(listeners) do
+				callback(player, pressed)
 			end
 		end
 	end
@@ -55,18 +55,20 @@ minetest.register_globalstep(function()
 	tas.array.forEach(minetest.get_connected_players(), callControlListeners)
 end)
 
+minetest.register_on_leaveplayer(function(player)
+	oldPlayerControls[player:get_player_name()] = nil
+end)
+
 --- Registers a control listener
 ---@param control tas.CONTROLS
 ---@param callback tas.ControlListenerCallback
 function tas.addControlListener(control, callback)
-	local listeners = controlListeners[keyName]
-	listeners[callback] = true
+	controlListeners[control][callback] = true
 end
 
 --- Unregisters a control listener.
 ---@param control tas.CONTROLS
 ---@param callback tas.ControlListenerCallback
 function tas.removeControlListener(control, callback)
-	local listeners = controlListeners[keyName]
-	listeners[callback] = nil
+	controlListeners[control][callback] = nil
 end
